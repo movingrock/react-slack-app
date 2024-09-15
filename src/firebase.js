2fac38b4a391d97732ccfe07a3b3115b5ccd3b50
movingrock <movingrock99@gmail.com>
2024-09-15 15:40:11 +0900

    feat: firebase database setting

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -7,6 +8,7 @@ import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyAZONBJkuh6HeQWVqyORRrh9Ph7Mol0Vu8",
   authDomain: "react-chat-app-278eb.firebaseapp.com",
+  databaseURL: "https://react-chat-app-278eb-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "react-chat-app-278eb",
   storageBucket: "react-chat-app-278eb.appspot.com",
   messagingSenderId: "895107984331",
@@ -15,5 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const db = getDatabase(app);
 
 export default app;
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,7 +1,105 @@
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
+import app, { db } from "../../firebase";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  updateProfile,
+} from "firebase/auth";
+import md5 from "md5";
+import { ref, set } from "firebase/database";
 
 const RegisterPage = () => {
-  return <div>RegisterPage</div>;
+  const auth = getAuth(app);
+  const [loading, setLoading] = useState(false);
+  const [errorFromSubmit, setErrorFromSubmit] = useState("");
+
+  const {
+    register,
+    watch,
+    formState: { errors },
+    handleSubmit,
+  } = useForm();
+
+  const onSubmit = async (data) => {
+    try {
+      const createdUser = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await updateProfile(auth.currentUser, {
+        displayName: data.name,
+        photoURL: `http://gravatar.com/avatar/${md5(
+          createdUser.user.email
+        )}?d=identicon`,
+      });
+
+      set(ref(db, `users/${createdUser.user.uid}`), {
+        name: createdUser.user.displayName,
+        image: createdUser.user.photoURL,
+      });
+    } catch (error) {
+      console.error(error);
+      setErrorFromSubmit(error.message);
+      setTimeout(() => {
+        setErrorFromSubmit("");
+      }, 3000);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="auth-wrapper">
+      <div style={{ textAlign: "center" }}>
+        <h3>Register</h3>
+      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          {...register("email", { required: true, pattern: /^\S+@\S+$/i })}
+        />
+        {errors.email && <p>This email field is required</p>}
+
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          {...register("name", { required: true, maxLength: 10 })}
+        />
+        {errors.name && errors.name.type === "required" && (
+          <p>This name field is required</p>
+        )}
+        {errors.name && errors.name.type === "maxLength" && (
+          <p>Your input exceed maximum length</p>
+        )}
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          {...register("password", { required: true, minLength: 6 })}
+        />
+        {errors.password && errors.password.type === "required" && (
+          <p>This password field is required</p>
+        )}
+        {errors.password && errors.password.type === "minLength" && (
+          <p>Password must have at least 6 characters</p>
+        )}
+        {errorFromSubmit && <p>{errorFromSubmit}</p>}
+        <input type="submit" disabled={loading} />
+        <Link to={"/login"} style={{ color: "gray", textDecoration: "none" }}>
+          이미 아이디가 있다면...
+        </Link>
+      </form>
+    </div>
+  );
 };
 
 export default RegisterPage;
