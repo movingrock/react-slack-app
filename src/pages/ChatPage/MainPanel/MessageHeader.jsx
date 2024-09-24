64432750bee25e7c058ada7b375b9bd136cd61a2
movingrock <movingrock99@gmail.com>
2024-09-24 19:01:45 +0900

    feat: Add DM

--- src/pages/ChatPage/MainPanel/MessageHeader.jsx
+++ src/pages/ChatPage/MainPanel/MessageHeader.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Col, FormControl, Image, InputGroup, Row } from "react-bootstrap";
 import { AiOutlineSearch } from "react-icons/ai";
+import { FaLock, FaLockOpen } from "react-icons/fa";
 import { useSelector } from "react-redux";
 
 const MessageHeader = ({ handleSearchChange }) => {
   const { currentChatRoom } = useSelector((state) => state.chatRoom);
+  const { isPrivateChatRoom } = useSelector((state) => state.chatRoom);
   return (
     <div
       style={{
@@ -17,21 +19,38 @@ const MessageHeader = ({ handleSearchChange }) => {
       }}
     >
       <Row>
-        <Col></Col>
+        <Col>
+          <h2>
+            {isPrivateChatRoom ? (
+              <FaLock style={{ marginBottom: 10 }} />
+            ) : (
+              <FaLockOpen style={{ marginBottom: 10 }} />
+            )}
+          </h2>
+        </Col>
         <Col>
           <InputGroup>
             <InputGroup.Text>
               <AiOutlineSearch />
             </InputGroup.Text>
-            <FormControl onChange={handleSearchChange} placeholder="Search Messages" />
+            <FormControl
+              onChange={handleSearchChange}
+              placeholder="Search Messages"
+            />
           </InputGroup>
         </Col>
       </Row>
 
-      <div style={{ display: "flex", justifyContent: "flex-end" }}>
-        <Image roundedCircle src={currentChatRoom?.createdBy.image} style={{ width: 30, height: 30, marginRight: 7 }} />
-        <p>{currentChatRoom?.createdBy.name}</p>
-      </div>
+      {!isPrivateChatRoom && (
+        <div style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Image
+            roundedCircle
+            src={currentChatRoom?.createdBy.image}
+            style={{ width: 30, height: 30, marginRight: 7 }}
+          />
+          <p>{currentChatRoom?.createdBy.name}</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/ChatPage/SidePanel/DirectMessages.jsx
+++ src/pages/ChatPage/SidePanel/DirectMessages.jsx
@@ -1,7 +1,94 @@
-import React from "react";
+import { onChildAdded, ref } from "firebase/database";
+import React, { useEffect, useState } from "react";
+import { FaRegSmile } from "react-icons/fa";
+import { db } from "../../../firebase";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  setCurrentChatRoom,
+  setPrivateChatRoom,
+} from "../../../store/chatRoomSlice";
 
 const DirectMessages = () => {
-  return <div>DirectMessages</div>;
+  const usersRef = ref(db, "users");
+
+  const [users, setUsers] = useState([]);
+  const [activeChatRoomId, setActiveChatRoomId] = useState("");
+  const [currentChatRoom, setCurrentChatRoom] = useState();
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (currentUser?.uid) addUsersListener(currentUser.uid);
+  }, [currentUser?.uid]);
+
+  const addUsersListener = (currentUserId) => {
+    let usersArray = [];
+
+    onChildAdded(usersRef, (DataSnapshot) => {
+      if (currentUserId !== DataSnapshot.key) {
+        let user = DataSnapshot.val();
+        user["uid"] = DataSnapshot.key;
+        usersArray.push(user);
+
+        const newUsersArray = [...usersArray];
+        setUsers(newUsersArray);
+      }
+    });
+  };
+
+  const getChatRoomId = (userId) => {
+    const currentUserId = currentUser.uid;
+
+    return userId > currentUserId
+      ? `${userId}/${currentUserId}`
+      : `${currentUserId}/${userId}`;
+  };
+
+  const changeChatRoom = (user) => {
+    const chatRoomId = getChatRoomId(user);
+    const chatRoomData = {
+      id: chatRoomId,
+      name: user.name,
+    };
+
+    dispatch(setCurrentChatRoom(chatRoomData));
+    dispatch(setPrivateChatRoom(true));
+    setActiveChatRoomId(user.uid);
+  };
+
+  const renderDirectMessages = (users) => {
+    users.length > 0 &&
+      users.map((user) => (
+        <li
+          key={user.uid}
+          style={{
+            backgroundColor: user.uid === activeChatRoomId ? "#ffffff45" : "",
+          }}
+          onClick={() => changeChatRoom(user)}
+        >
+          # {user.name}
+        </li>
+      ));
+  };
+
+  return (
+    <div>
+      <span
+        style={{
+          position: "relative",
+          width: "100%",
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <FaRegSmile style={{ marginRight: 5 }} />
+        DIRECT MESSAGS
+      </span>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {renderDirectMessages(users)}
+      </ul>
+    </div>
+  );
 };
 
 export default DirectMessages;
